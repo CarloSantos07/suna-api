d85123726e4f8767256aba0f9b3daeebe1df25b0
msjlwhite <msjlwhite@gmail.com>
2020-01-29 14:30:26 -0600

    updated model associations and validations

--- app/models/note.rb
+++ app/models/note.rb
@@ -1,3 +1,10 @@
 class Note < ApplicationRecord
-  belongs_to :team
+  # belongs_to :team
+  belongs_to :stand_up
+
+  # create validations here
+  validates :employee, presence: true
+  validates :type, presence :true
+  validates :body, presence :true
+  validates :stand_up, presence :true, length: { maximum: 36 }, uuid: true
 end
--- app/models/stand_up.rb
+++ app/models/stand_up.rb
@@ -1,2 +1,18 @@
 class StandUp < ApplicationRecord
+belongs_to :team
+has_many :notes, class_name: "note", foreign_key: "reference_id"
+# has_many :notes, through: :team
+
+# put validations here
+before_validation :set_uuid, on: :create
+validates :id, presence: true, length: { maximum: 36 }, uuid: true
+
+  def set_uuid
+    self.id = SecureRandom.uuid
+  end
+
+validates :date, presence: true
+validates :scrum_master, presence: true
+validates :team, presence: true
+
 end
