3de46d3cc9f213aadccfd078bc69164e74dcf9fc
msjlwhite <msjlwhite@gmail.com>
2020-02-10 18:16:31 -0600

    added factories for notes and stand_ups to help with rspec testing

---
+++ spec/factories/notes.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :note do
+    employee { "MyString" }
+    kind { 1 }
+    body { "MyText" }
+    stand_up_id_id { "MyString" }
+  end
+end
---
+++ spec/factories/stand_ups.rb
@@ -0,0 +1,8 @@
+FactoryBot.define do
+  factory :stand_up do
+    id { "MyString" }
+    date { "2020-02-10 16:54:31" }
+    scrum_master { "MyString" }
+    pod_id { "MyString" }
+  end
+end
