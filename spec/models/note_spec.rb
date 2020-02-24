f6a6be8df2f7a0fa01900b8ec8a3d4dcafafe501
msjlwhite <msjlwhite@gmail.com>
2020-02-24 16:16:14 -0600

    suna-api updated and tested the rspec model scenarios for StandUp and Note

--- spec/models/note_spec.rb
+++ spec/models/note_spec.rb
@@ -2,35 +2,80 @@ require 'rails_helper'
 
 RSpec.describe Note, type: :model do
 
-  describe 'positive model testing' do
-    # let!(:stand) {create(:stand_up, id: '1')}
-    # let!(:note) { create(:note, stand_up_id: stand.id) }
-
-    # binding.pry
-    # let!(:stand) { create(:stand_up) }
-    # let!(:note) { create(:note, stand_up_id: stand.id) }
-    stand = FactoryBot.create(:stand_up)
-    note = FactoryBot.create(:note, employee: Faker::FunnyName.name, stand_up_id: stand.id)
-    binding.pry
-
-    it 'verifies the presence of an id' do
-      expect(note.id).not_to be_blank
-      expect(note.id).not_to be_nil
-    end
+  describe 'positive generic spec tests for the model' do
+    stand = FactoryBot.create(:stand_up, date: Time.now)
+    note = FactoryBot.create(:note, employee: Faker::FunnyName.name, kind: 1, stand_up_id: stand.id)
 
     it 'verifies the presence of employee name' do
-      # expect(note.employee).to be('name')
       expect(note.employee).not_to be_nil
       expect(note.employee).not_to be_empty
     end
 
     it 'verifies the presence of the kind of note' do
-      # binding.pry
       expect(note.kind).not_to be_blank
       expect(note.kind).not_to be_nil
       expect(note.kind).not_to be_empty
     end
-
   end
 
+  describe 'negative generic spec tests for the model' do
+    stand = FactoryBot.create(:stand_up, date: Time.now)
+    note = FactoryBot.create(:note, employee: '', kind: 'today', stand_up_id: stand.id)
+
+    it 'verifies the absence of employee name' do
+      expect(note.employee).to be_blank
+      expect(note.employee).to be_empty
+    end
+
+    it 'verifies the absence of note body' do
+      expect(note.body).not_to be_empty
+      expect(note.body).not_to be_blank
+    end
+  end
+
+  describe 'positive specific/explicit spec tests for the model' do
+    stand = FactoryBot.create(:stand_up, id: SecureRandom.uuid, scrum_master: 'Ben N. Gerries', pod_id: '00000001')
+    note = FactoryBot.create(:note, employee: 'Ben N. Gerries', kind: 'roadblocks', body: Faker::TvShows::StrangerThings.quote, stand_up_id: stand.id)
+
+    it 'verify the presence of a employee name' do
+      expect(note.employee).to eq('Ben N. Gerries')
+    end
+
+    it 'verify the presence of a kind of note' do
+      expect(note.kind).not_to eq('yesterday')
+      expect(note.kind).not_to eq('today')
+      expect(note.kind).to eq('roadblocks')
+    end
+
+    it "verify the presence of information in the note's body" do
+      expect(note.body).not_to eq('')
+      expect(note.body).not_to be_blank
+      expect(note.body).not_to be_empty
+    end
+  end
+
+  describe 'negative specific/explicit spec tests for the model' do
+    stand = FactoryBot.create(:stand_up, id: SecureRandom.uuid, scrum_master: 'Ben N. Gerries', pod_id: '00000001')
+    # note = FactoryBot.create(:note, employee: 'Ben N. Gerries', kind: 'roadblocks', body: Faker::TvShows::StrangerThings.quote, stand_up_id: stand.id)
+    note = FactoryBot.create(:note, employee: 'Ben N. Gerries', kind: 'roadblocks', body: 'Heey!', stand_up_id: stand.id)
+
+
+    it 'verify that the employee name is valid' do
+      expect(note.employee).not_to eq('')
+    end
+
+    it 'verify that the kind of note has a valid type' do
+      expect(note.kind).not_to eq('yesteryear')
+      expect(note.kind).not_to eq('whenever')
+    end
+
+    it 'verify the note contains some kind of information' do
+      expect(note.body).not_to eq('')
+      expect(note.body).not_to eq('hey')
+      expect(note.body).not_to eq('hey!')
+      expect(note.body).not_to be_blank
+      expect(note.body).not_to be_empty
+    end
+
+  end
 end
--- spec/models/stand_up_spec.rb
+++ spec/models/stand_up_spec.rb
@@ -2,20 +2,15 @@ require 'rails_helper'
 require 'faker'
 
 RSpec.describe StandUp, type: :model do
-  describe 'positive model spec tests' do
-    # let!(:stand) { StandUp.new(id: SecureRandom.uuid, date: Time.now, scrum_master: 'Rich', pod_id: Faker::Hipster.word) }
-    # let!(:stand) { create(:stand_up) }
 
-    stand = FactoryBot.create(:stand_up)
-    
-    binding.pry
-    
+  describe 'positive generic spec tests for the model' do
+    stand = FactoryBot.create(:stand_up, date: Time.now)
 
-    it 'verify the presence of id' do
-      expect(stand.id).not_to be_empty
-      expect(stand.id).not_to be_blank
-      expect(stand.id).not_to be_nil
-    end
+    # it 'verify the presence of id' do
+    #   expect(stand.id).not_to be_empty
+    #   expect(stand.id).not_to be_blank
+    #   expect(stand.id).not_to be_nil
+    # end
 
     it 'verify the length of id' do
       expect(stand.id.length).to be <= 36
@@ -36,7 +31,20 @@ RSpec.describe StandUp, type: :model do
     end
   end
 
-  describe 'negative model spec tests' do
+  describe 'positive specific/explicit spec tests for the model' do
+    stand = FactoryBot.create(:stand_up, id: SecureRandom.uuid, scrum_master: 'Ben N. Gerries', pod_id: '00000001')
+
+    it 'verify the presence of a scrum master' do
+      expect(stand.scrum_master).to eq('Ben N. Gerries')
+    end
+
+
+    it 'verify the presence of a pod id' do
+      expect(stand.pod_id).to eq('00000001')
+    end
+  end
+
+  describe 'negative generic spec tests for the model' do
     let!(:stand) { StandUp.new(id: '', date: '', scrum_master: '', pod_id: '') }
 
     it 'verify that when no id is given its value will be empty or blank' do
@@ -62,4 +70,27 @@ RSpec.describe StandUp, type: :model do
       expect(stand.pod_id).to be_empty
     end
   end
+
+  describe 'negative specific/explicit spec tests for the model' do
+    let!(:stand) { StandUp.new(id: '', date: '', scrum_master: '', pod_id: '') }
+
+    it 'verify that when no id is given the length of id is 0 ' do
+      expect(stand.id.length).to eq(0)
+    end
+
+    it 'verify that when no date is given its value will be nil/blank' do
+      expect(stand.date).to be_nil
+      expect(stand.date).to be_blank
+    end
+
+    it 'verify that when no scrum master has been entered then its value will be blank/empty' do
+      expect(stand.scrum_master).to be_empty
+      expect(stand.scrum_master).to be_blank
+    end
+
+    it 'verify that when no pod id is given then its value will be empty' do
+      expect(stand.pod_id).to be_empty
+    end
+  end
+
 end
