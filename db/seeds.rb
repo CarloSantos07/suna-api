31d52948b97eb61c9bae6d8f5fd6300f57ccef34
msjlwhite <msjlwhite@gmail.com>
2020-03-03 17:29:05 -0600

    suna-api updated seed.rb file to add records to the database for testing the controllers and routes.

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,18 +7,72 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 # WORKING!!!!! PRAISE THE LORD!!!
-4.times do
-  stand = StandUp.new(
+# 4.times do
+#   stand = StandUp.new(
+#     date: Time.now,
+#     scrum_master: Faker::FunnyName.name,
+#     pod_id: Faker::Number.number(digits: 8),
+#   )
+#   stand.save
+
+#   Note.create!(
+#     employee: stand.scrum_master,
+#     kind: Faker::Number.within(range: 0..2),
+#     body: Faker::TvShows::StrangerThings.quote,
+#     stand_up_id: stand.id
+#   )
+# end
+
+# 4.times do
+#   stand = StandUp.new(
+#     date: Time.now,
+#     scrum_master: Faker::FunnyName.name,
+#     pod_id: Faker::Number.number(digits: 8),
+#   )
+#   stand.save
+
+#   Note.create!(
+#     employee: Faker::TvShows::Community.characters,
+#     kind: Faker::Number.within(range: 0..2),
+#     body: Faker::TvShows::Community.quotes,
+#     stand_up_id: stand.id
+#   )
+# end
+
+# created Stand up with a single note for each (3) different employees
+# 4.times do
+#   stand = StandUp.new(
+#     date: Time.now,
+#     scrum_master: Faker::FunnyName.name,
+#     pod_id: Faker::Number.number(digits: 8),
+#   )
+#   stand.save
+
+#   3.times do
+#     Note.create!(
+#       employee: Faker::TvShows::Community.characters,
+#       kind: Faker::Number.within(range: 0..2),
+#       body: Faker::TvShows::Community.quotes,
+#       stand_up_id: stand.id
+#     )
+#   end
+# end
+5.times do
+stand = StandUp.new(
     date: Time.now,
     scrum_master: Faker::FunnyName.name,
     pod_id: Faker::Number.number(digits: 8),
   )
   stand.save
 
-  Note.create!(
-    employee: stand.scrum_master,
-    kind: Faker::Number.within(range: 0..2),
-    body: Faker::TvShows::StrangerThings.quote,
-    stand_up_id: stand.id
-  )
+  3.times do
+    Note.create!(
+      employee: Faker::TvShows::NewGirl.character,
+      # Faker::TvShows::Community.characters,
+      kind: Faker::Number.within(range: 0..2),
+      body: Faker::TvShows::NewGirl.quote
+      # Faker::TvShows::Community.quotes,
+      stand_up_id: stand.id
+    )
+  end
 end
